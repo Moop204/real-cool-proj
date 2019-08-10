aa5fe4872d383cc05e120f3ff70dffa4f62e84a6
z5164929 <z5164929@student.unsw.edu.au>
2019-08-11 00:02:58 +1000

    calculates final value

--- backend/nlp_test.py
+++ backend/nlp_test.py
@@ -20,6 +20,7 @@ with open("positive.txt","r") as f:
 
 print("------------------------------")
 
+rev_data1 = score/total
 print("Generally good review: {}".format(score/(total)))
 print("Total: {}".format(total))
 print("Score: {}".format((score)))
@@ -34,26 +35,43 @@ with open("negative.txt","r") as f:
         if (vs['compound'] != 0):
         	score = score+vs['compound']
         	total = total + 1
-
+rev_data2 = score/total
 print("Generally bad review: {}".format(score/(total)))
 print("Total: {}".format(total))
 print("Score: {}".format((score)))
 
 
+#Secne 1: priority
 #normalise data out from 0 - 1
-food = 1			#average rating out of 5
-school = 2			#if greater than 5 schools -> 100 percetage
-ent = 3				#if greater than 5 placed -> 100 percentage
-review = 4			#review data -> make review data percentage 0->2 out of 2
+food1 = 1			#average rating out of 5
+school1 = 2			#if greater than 5 schools -> 100 percetage
+ent1 = 3				#if greater than 5 placed -> 100 percentage
+review1 = 4			#review data -> make review data percentage 0->2 out of 2
 
-#no. of max school
-#no. of ent areas
-
+food_data = (4/5)
+school = 3/5
+ent = 1/5
 
 #max total is 50 - lowest 0 
-total = review* rev_data + ent* ent_data + school* school_data + food*food_data
+total1 = review1* rev_data1 + ent1* ent_data + school1* school_data + food1*food_data
 
 
+
+#scene2: priority
+food2 = 3
+school2 = 1 
+ent2 = 2
+review2 = 4 
+
+food_data = (1/5)
+school = 2/5
+ent = 4/5
+
+total2 = review2* rev_data2 + ent2* ent_data + school2* school_data + food2*food_data
+
+
+print("Total1: {}".format(total1))
+print("Total2: {}".format(total2))
 
 
 
